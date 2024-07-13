ddd1dc9a520b983319fc0447df8d9fa7fa0edd50
codegreen <codegreen19s@gmail.com>
2024-07-13 09:52:43 +0600

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+      dolorum libero accusamus laudantium voluptatum sed officia, corrupti ex ad
+      ipsam deserunt odio, quos, illo qui reiciendis delectus necessitatibus
+      mollitia repudiandae. Footer...
+    </div>
+  );
+};
+
+export default Footer;
