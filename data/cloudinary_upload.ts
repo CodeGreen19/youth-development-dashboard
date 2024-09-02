b0df51621f42de8520efb16a5e3d37639c9a8764
codegreen <codegreen19s@gmail.com>
2024-09-02 16:32:12 +0600

    again tasting

--- data/cloudinary_upload.ts
+++ data/cloudinary_upload.ts
@@ -1,5 +1,13 @@
-import cloudinary from "@/lib/cloudinary";
+"use server";
+
 import { ImageUrlType } from "@/types";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export const uploadtoCloud = async ({
   file,
